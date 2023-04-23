7e3d64e880e2765a67a72d91c6391762fae1d9c8
daeho sim <tlaeogh5@gmail.com>
2023-04-23 21:21:31 +0900

    Edit: in app

--- src/app/apple/page.tsx
+++ src/app/apple/page.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function apple() {
   return <h1>apple!</h1>;
 }
--- src/app/fruits/page.tsx
+++ src/app/fruits/page.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function FruitsPage() {
   return <h1>과일소개 페이지!</h1>;
 }
