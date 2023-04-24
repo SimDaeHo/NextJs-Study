c2ecf07ea315d9dfccf972d67dfa11fdcebd8e51
daeho sim <tlaeogh5@gmail.com>
2023-04-24 19:46:47 +0900

    동적 라우트 페이지 미리 만들기

--- src/app/fruits/[slug]/page.tsx
+++ src/app/fruits/[slug]/page.tsx
@@ -7,3 +7,10 @@ type Props = {
 export default function bananaPage({ params }: Props) {
   return <h1>{params.slug} 설명 페이지!</h1>;
 }
+
+export function generateStaticParams() {
+  const fruits = ["apple", "banana"];
+  return fruits.map((fruits) => ({
+    slug: fruits,
+  }));
+}
