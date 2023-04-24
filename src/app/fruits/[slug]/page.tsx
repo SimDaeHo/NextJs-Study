4b4d6bd319b2b786882c8b5b8f565361db5f2ab0
daeho sim <tlaeogh5@gmail.com>
2023-04-24 16:23:52 +0900

    Edit: slug-props

--- src/app/fruits/[slug]/page.tsx
+++ src/app/fruits/[slug]/page.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+type Props = {
+  params: {
+    slug: string;
+  };
+};
 
-export default function bananaPage() {
+export default function bananaPage({ params }: Props) {
   return <h1>바나나 설명 페이지!</h1>;
 }
