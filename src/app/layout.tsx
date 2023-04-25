5163722dc3b484fd222adb3f4083b9e60206fcd3
daeho sim <tlaeogh5@gmail.com>
2023-04-25 21:25:00 +0900

    layout page 만들기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
-import './globals.css'
+import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       {/*
@@ -12,7 +8,15 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <header>
+          <h1>Daeho Memo</h1>
+          <nav>
+            <a href="">Apple</a>
+            <a href="">Banana</a>
+          </nav>
+        </header>
+      </body>
     </html>
-  )
+  );
 }
