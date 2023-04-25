a4ef66063e220f5284d2a3613cd6186479aed171
daeho sim <tlaeogh5@gmail.com>
2023-04-25 21:30:49 +0900

    children 넣어줌

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <a href="">Banana</a>
           </nav>
         </header>
+        {children}
       </body>
     </html>
   );
