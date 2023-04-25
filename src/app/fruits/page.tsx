b75b9ecd8606e4908d117ea753abdb159c87341e
daeho sim <tlaeogh5@gmail.com>
2023-04-26 01:11:52 +0900

    fruits page 안에 Link 추가하기

--- src/app/fruits/page.tsx
+++ src/app/fruits/page.tsx
@@ -1,3 +1,23 @@
+import Link from "next/link";
+
 export default function FruitsPage() {
-  return <h1>과일소개 페이지!</h1>;
+  return (
+    <>
+      <h1>과일소개 페이지!</h1>
+      <ul>
+        <Link href="/fruits/orange">
+          <li>orange</li>
+        </Link>
+        <Link href="/fruits/melon">
+          <li>melon</li>
+        </Link>
+        <Link href="/fruits/peach">
+          <li>peach</li>
+        </Link>
+        <Link href="/fruits/lemon">
+          <li>lemon</li>
+        </Link>
+      </ul>
+    </>
+  );
 }
