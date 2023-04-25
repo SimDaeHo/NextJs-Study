dfdffdb3cbc8d7c325167573353d51de02d06ddf
daeho sim <tlaeogh5@gmail.com>
2023-04-26 01:00:45 +0900

    fruits 폴더 안에 Link 설정

--- src/app/fruits/layout.tsx
+++ src/app/fruits/layout.tsx
@@ -1,11 +1,12 @@
+import Link from "next/link";
 import styles from "./layout.module.css";
 
 export default function FruitsLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <nav className={styles.nav}>
-        <a href="">사과</a>
-        <a href="">바나나</a>
+        <Link href="/fruits/apple">사과</Link>
+        <Link href="/fruits/banana">바나나</Link>
       </nav>
       <section className={styles.fruit}>{children}</section>
     </>
