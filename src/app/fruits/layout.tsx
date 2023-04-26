c210d7ac42207c96c42e1dd9726e1c318a259b92
daeho sim <tlaeogh5@gmail.com>
2023-04-27 02:02:55 +0900

    fruits metadata

--- src/app/fruits/layout.tsx
+++ src/app/fruits/layout.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
 import styles from "./layout.module.css";
 
+export const metadata = {
+  title: "맛있는 과일 정보 사이트 | 전체 과일 확인",
+  description: "맛있는 과일을 구경해 보세요.",
+};
+
 export default function FruitsLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
