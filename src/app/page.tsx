359ad27f964a2a14919fea338ba3b12f3b2d61d1
daeho sim <tlaeogh5@gmail.com>
2023-04-27 21:16:08 +0900

    클라이언트 컴포넌트 만들기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,19 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
 import os from "os"; // 노드 APIs
+import { useState } from "react";
+import Counter from "@/components/Counter";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   console.log("과일!");
   console.log(os.hostname());
-  return <h1>홈페이지다!!</h1>;
+
+  return (
+    <>
+      <h1>홈페이지다!!</h1>;
+      <Counter />
+    </>
+  );
 }
