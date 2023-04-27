a3ef8ee72506d508a4ab4b089e193822e354dd74
daeho sim <tlaeogh5@gmail.com>
2023-04-27 21:31:35 +0900

    클라이언트 컴포넌트 count 예시

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
   return (
     <>
-      <h1>홈페이지다!!</h1>;
+      <h1>홈페이지다!!</h1>
       <Counter />
     </>
   );
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Counter() {
-  return <></>;
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <p>{count}</p>
+      <button onClick={() => setCount((num) => num + 1)}>숫자 증가 버튼</button>
+    </>
+  );
 }
