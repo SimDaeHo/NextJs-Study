96f41460767ae198bbb47d881cc9cc8de5473ba7
daeho sim <tlaeogh5@gmail.com>
2023-04-27 20:56:18 +0900

    노드 모듈 os 확인

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
+import os from "os"; // 노드 APIs
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   console.log("과일!");
+  console.log(os.hostname());
   return <h1>홈페이지다!!</h1>;
 }
