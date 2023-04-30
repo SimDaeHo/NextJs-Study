7bae6eac51b8147ed4c7e3d42be9ea62153c62c7
daeho sim <tlaeogh5@gmail.com>
2023-04-30 21:51:03 +0900

    미들웨어 경로 리다이렉팅

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,9 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
   console.log("미들웨어가 실행되고 있음! 체크중!@@");
+  if (request.nextUrl.pathname.startsWith("/fruits/1004")) {
+    console.log("미들웨어 - 경로를 리다이렉팅함!");
+    return NextResponse.redirect(new URL("fruits", request.url));
+  }
 }
