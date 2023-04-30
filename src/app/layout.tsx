18c6a50a10b0ea8b826a3d66c190c4af4c2ac5b7
daeho sim <tlaeogh5@gmail.com>
2023-04-30 17:55:48 +0900

    font 설정 하기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,14 @@ import Link from "next/link";
 import "./globals.css";
 import styles from "./layout.module.css";
 
+import { Open_Sans } from "@next/font/google";
+import { Nanum_Gothic } from "@next/font/google";
+
+const sans = Open_Sans({ subsets: ["latin"] });
+const gothic = Nanum_Gothic({
+  weight: "700",
+  subsets: ["latin"],
+});
 export const metadata = {
   title: "맛있는 과일 정보 사이트",
   description: "맛있는 과일을 판매하는 곳입니다.",
@@ -12,7 +20,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={sans.className}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
@@ -20,7 +28,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head />
       <body>
         <header className={styles.header}>
-          <h1>Daeho Memo</h1>
+          <h1 className={gothic.className}>Daeho Memo</h1>
           <nav className={styles.nav}>
             <Link href="/fruits">Fruits</Link>
             <Link href="/apple">Apple</Link>
