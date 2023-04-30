ccc2e4ff10fe03592f5a5870ef815e80449049e8
daeho sim <tlaeogh5@gmail.com>
2023-04-30 19:45:43 +0900

    redirect 만들기

--- next.config.js
+++ next.config.js
@@ -11,6 +11,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/fruits/deleted",
+        destination: "/fruits",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
